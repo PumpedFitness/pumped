9512f798df4b682f7f2688a63239e9fbf1c365d6
devin <devinfritz@gmx.de>
2026-05-02 11:55:08 +0200

    ESLint config

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: [
+    'src/data/api/generated.ts',
+    'src/data/local/schema.generated.ts',
+    'src/data/local/schema.enums.generated.ts',
+    'scripts/',
+    'design_handoff_pumped*/',
+  ],
 };
